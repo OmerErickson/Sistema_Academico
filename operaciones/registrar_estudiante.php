718a25a4908842942d557305a4f24e88b3d33380
OmerErickson <omertaboadaiestph@gmail.com>
2022-12-12 16:44:00 -0500

    estudiantessss

--- operaciones/registrar_estudiante.php
+++ operaciones/registrar_estudiante.php
@@ -2,24 +2,24 @@
 include "../include/conexion.php";
 include "../include/busquedas.php";
 $dni = $_POST['dni'];
-$nom_ap = $_POST['nom_ap'];
+$apellidos_nombres = $_POST['apellidos_nombres'];
 $id_genero = $_POST['id_genero'];
 $fecha_nac = $_POST['fecha_nac'];
 $direccion = $_POST['direccion'];
-$email = $_POST['email'];
-$cel = $_POST['telefono'];
+$correo = $_POST['correo'];
+$telefono = $_POST['telefono'];
 $anio_ingreso = $_POST['anio_ingreso'];
-$programa_estudio = $_POST['programa_estudio'];
-$semestre = $_POST['semestre'];
+$id_programa_estudios = $_POST['id_programa_estudios'];
+$id_semestre = $_POST['id_semestre'];
 $seccion = $_POST['seccion'];
 $turno = $_POST['turno'];
-$condicion = $_POST['condicion'];
+$id_condicion = $_POST['id_condicion'];
 $discapacidad = $_POST['discapacidad'];
 
 $b_estudiante = buscarEstudianteByDni($conexion, $dni);
 $c_r_b_estudiante = mysqli_num_rows($b_estudiante);
 if ($c_r_b_estudiante == 0) {//validamos que no haya registros en la base de datos
-	$insertar = "INSERT INTO estudiante (dni, apellidos_nombres, id_genero, fecha_nac, direccion, correo, telefono, anio_ingreso, id_programa_estudios, id_semestre, seccion, turno, id_condicion, discapacidad) VALUES ('$dni','$nom_ap','$id_genero', '$fecha_nac', '$direccion', '$email', '$cel', '$anio_ingreso', '$programa_estudio', '$semestre', '$seccion', '$turno', '$condicion', '$discapacidad')";
+	$insertar = "INSERT INTO estudiante (dni, apellidos_nombres, id_genero, fecha_nac, direccion, correo, telefono, anio_ingreso, id_programa_estudios, id_semestre, seccion, turno, id_condicion, discapacidad) VALUES ('$dni','$apellidos_nombres','$id_genero', '$fecha_nac', '$direccion', '$correo', '$telefono', '$anio_ingreso', '$id_programa_estudios', '$id_semestre', '$seccion', '$turno', '$id_condicion', '$discapacidad')";
 	$ejecutar_insetar = mysqli_query($conexion, $insertar);
 	// registrar usuario
 	$b_id_estudiante = buscarEstudianteByDni($conexion, $dni);
