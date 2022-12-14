de1864437c037d32f95c6704f56fa0e4fbfb0afa
OmerErickson <omertaboadaiestph@gmail.com>
2022-12-14 17:44:37 -0500

    Update usuario_docente.php

--- usuario_docente.php
+++ usuario_docente.php
@@ -41,7 +41,7 @@ include "include/verificar_sesion.php";
               <div class="col-md-12 col-sm-12 col-xs-12">
                 <div class="x_panel">
                   <div class="x_title">
-                    <h2>Registro de Semestre</h2>
+                    <h2>Registro de Usuario Docente</h2>
                     
                     <div class="clearfix"></div>
                   </div>
