39c98cbe20d806352c38b861d8db670e9db87096
OmerErickson <omertaboadaiestph@gmail.com>
2022-12-14 16:37:18 -0500

    nr

--- operaciones/registrar_modulo_profesional.php
+++ operaciones/registrar_modulo_profesional.php
@@ -2,19 +2,20 @@
 include "../include/conexion.php";
 include "../include/busquedas.php";
 
-$descripcion     = $_POST['descripcion'];
+$descrip     = $_POST['descripcion'];
 $nro_modulo       = $_POST['nro_modulo'];
 $id_programa_estudio     = $_POST['id_programa_estudio'];
 
 
-$b_descripcion  = buscarModuloProfesional($conexion, $descripcion );
-$c_r_b_descripcion  = mysqli_num_rows($b_descripcion );
+$b_descrip  = buscarProgramaEstudio($conexion, $descrip );
+$c_r_b_descrip  = mysqli_num_rows($b_descrip );
 if ($c_r_b_programa_estudios == 0) {
-	$insertar = "INSERT INTO programa_estudios (descripcion, nro_modulo, id_programa_estudio) VALUES ('$descripcion','$nro_modulo','$id_programa_estudio')";
-
+	$insertar = "INSERT INTO programa_estudios (descripcion, nro_modulo, id_programa_estudio) VALUES ('$descrip','$nro_modulo','$id_programa_estudio')";
 	$ejecutar_insetar = mysqli_query($conexion, $insertar);
 	
-	if ($insetar) {
+
+
+	if ($ejecutar_insetar) {
 		echo "<script>
                 alert('Registro Exitoso programa de estudios');
                 window.location= '../modulo_profesional.php'
