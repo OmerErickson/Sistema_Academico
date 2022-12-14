16f0d6edfc77bdd2621442ea6632ba59043bc17c
OmerErickson <omertaboadaiestph@gmail.com>
2022-12-14 12:28:09 -0500

    Update registrar_periodo_academico.php

---
+++ operaciones/registrar_periodo_academico.php
@@ -0,0 +1,41 @@
+<?php
+
+include "../include/conexion.php";
+include "../include/busquedas.php";
+
+$nombre= $_POST['nombre'];
+$fecha_inicio= $_POST['fecha_inicio'];
+$fecha_fin= $_POST['fecha_fin'];
+$director= $_POST['director'];
+$fecha_actas= $_POST['fecha_actas'];
+
+
+$b_nombre = buscarPeriodoAcademicoByNombre($conexion, $nombre );
+$c_r_b_nombre$b_nombre = mysqli_num_rows($b_nombre);
+if ($c_r_b_nombre == 0) {
+	$insertar = "INSERT INTO periodo_academico (nombre, fecha_inicio, fecha_fin, director, fecha_actas) VALUES ('$nombre', '$fecha_inicio', '$fecha_fin', '$director', '$fecha_actas')";
+	$ejecutar_insetar = mysqli_query($conexion, $insertar);
+
+    $b_id_nombre = buscarPeriodoAcademicoByNombre($conexion, $b_nombre);
+	$res_b_nombre = mysqli_fetch_array( $b_id_nombre);
+
+
+
+	
+	if ($insertar) {
+		echo "<script>
+                alert('Registro de periodo academico Exitoso');
+                window.location= '../periodo_academicos.php'
+    			</script>";
+	}else{
+		echo "<script>
+			alert('Error al Registrar periodo academico');
+			window.history.back();
+			</script>
+			";
+	}
+
+}
+
+
+?>
