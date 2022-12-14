0ffeba81579b29b8899e508100e87780a5e1a509
OmerErickson <omertaboadaiestph@gmail.com>
2022-12-14 16:59:29 -0500

    uni

--- operaciones/registrar_unidad_didactica.php
+++ operaciones/registrar_unidad_didactica.php
@@ -2,7 +2,7 @@
 include "../include/conexion.php";
 include "../include/busquedas.php";
 
-$descripcion     = $_POST['descripcion'];
+$descrip     = $_POST['descripcion'];
 $id_programa_estudio       = $_POST['id_programa_estudio'];
 $id_modulo     = $_POST['id_modulo_profesional'];
 $id_semestre = $_POST['id_semestre'];
@@ -11,15 +11,19 @@ $horas = $_POST['horas'];
 $tipo = $_POST['tipo'];
 $orden = $_POST['orden'];
 
-$b_descripcion  = buscarUnidadDidactica($conexion, $descripcion);
-$c_r_b_descripcion  = mysqli_num_rows($b_descripcion );
-if ($c_r_b_programa_estudios == 0) {
-
-	$insertar = "INSERT INTO programa_estudios (descripcion, id_programa_estudio, id_modulo, id_semestre, creditos, horas, tipo, orden) VALUES ('$descripcion','$id_programa_estudio','$id_modulo', '$id_semestre', '$creditos', '$horas', '$tipo', '$orden')";
+$b_descrip  = buscarPresentePeriodoAcademico($conexion, $descrip );
+$c_r_b_descrip  = mysqli_num_rows($b_descrip );
+if ($c_r_b_id_programa_estudios == 0) {
+	$insertar = "INSERT INTO unidad_didactica (escrip, id_programa_estudio, id_modulo_profesional, id_semestre, creditos, horas, tipo, orden) VALUES ('$descrip ','$id_programa_estudio','$id_modulo', '$id_semestre', '$creditos', '$horas', '$tipo', '$orden')";
 
 	$ejecutar_insetar = mysqli_query($conexion, $insertar);
+
+    
+
+
+
 	
-	if ($ejecutar_insetar) {
+	if ($insertar ) {
 		echo "<script>
                 alert('Registro Exitoso de Unidad Didactica');
                 window.location= '../unidad_didacticas.php'
@@ -31,6 +35,7 @@ if ($c_r_b_programa_estudios == 0) {
 			</script>
 			";
 	}
+
 }
 
 
