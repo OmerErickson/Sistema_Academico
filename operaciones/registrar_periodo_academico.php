02faed455bd8b82d808dfb5ec5bbe48f4242e32e
OmerErickson <omertaboadaiestph@gmail.com>
2022-12-14 17:18:31 -0500

    d

--- operaciones/registrar_periodo_academico.php
+++ operaciones/registrar_periodo_academico.php
@@ -8,19 +8,19 @@ $fecha_fin= $_POST['fecha_fin'];
 $director= $_POST['director'];
 $fecha_actas= $_POST['fecha_actas'];
 
-$b_nombre = buscarPeriodoAcademicoByName($conexion, $nombre);
+$b_nombre = buscarPeriodoAcademicoByNombre($conexion, $nombre);
 $c_r_b_nombre= mysqli_num_rows($b_nombre);
 
 if ($c_r_b_nombre == 0) {
-	echo "hola";
+	
 	$insertar = "INSERT INTO periodo_academico(nombre, fecha_inicio, fecha_fin, director, fecha_actas) VALUES ('$nombre','$fecha_inicio','$fecha_fin','$director','$fecha_actas')";
 	
 	$ejecutar_insetar = mysqli_query($conexion, $insertar);
 	
-    $b_id_nombre = buscarPeriodoAcademicoByName($conexion, $nombre);
+    $b_id_nombre = buscarPeriodoAcademicoByNombre($conexion, $nombre);
 	$res_b_nombre = mysqli_fetch_array($b_id_nombre);
 	
-	// registrar usuario
+	
 	if ($insertar) {
 		echo "<script>
                 alert('Registro Exitoso periodo academico');
