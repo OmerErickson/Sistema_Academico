e1519ffde6297805d11648323d97a4792e942278
OmerErickson <omertaboadaiestph@gmail.com>
2022-12-14 11:01:10 -0500

    Update dato_institucional.php

--- dato_institucional.php
+++ dato_institucional.php
@@ -40,7 +40,7 @@ include "include/verificar_sesion.php";
               <div class="col-md-12 col-sm-12 col-xs-12">
                 <div class="x_panel">
                   <div class="x_title">
-                    <h2>Registro de Datos Instirucionales</h2>
+                  <h2 align ="center">Datos Institucionales</h2>
                     
                     <div class="clearfix"></div>
                   </div>
