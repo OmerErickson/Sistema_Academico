847c68817b231297e7134a733de763ba80f2c49a
OmerErickson <omertaboadaiestph@gmail.com>
2022-12-14 17:17:13 -0500

    Update registrar_periodo_academico.php

--- operaciones/registrar_periodo_academico.php
+++ operaciones/registrar_periodo_academico.php
@@ -1,5 +1,4 @@
 <?php
-
 include "../include/conexion.php";
 include "../include/busquedas.php";
 
@@ -9,28 +8,27 @@ $fecha_fin= $_POST['fecha_fin'];
 $director= $_POST['director'];
 $fecha_actas= $_POST['fecha_actas'];
 
+$b_nombre = buscarPeriodoAcademicoByName($conexion, $nombre);
+$c_r_b_nombre= mysqli_num_rows($b_nombre);
 
-$b_nombre = buscarPeriodoAcademicoByNombre($conexion, $nombre );
-$c_r_b_nombre = mysqli_num_rows($b_nombre);
 if ($c_r_b_nombre == 0) {
-	$insertar = "INSERT INTO periodo_academico (nombre, fecha_inicio, fecha_fin, director, fecha_actas) VALUES ('$nombre', '$fecha_inicio', '$fecha_fin', '$director', '$fecha_actas')";
-
+	echo "hola";
+	$insertar = "INSERT INTO periodo_academico(nombre, fecha_inicio, fecha_fin, director, fecha_actas) VALUES ('$nombre','$fecha_inicio','$fecha_fin','$director','$fecha_actas')";
+	
 	$ejecutar_insetar = mysqli_query($conexion, $insertar);
-
+	
     $b_id_nombre = buscarPeriodoAcademicoByName($conexion, $nombre);
 	$res_b_nombre = mysqli_fetch_array($b_id_nombre);
-
-
-
 	
-	if ($ejecutar_insetar) {
+	// registrar usuario
+	if ($insertar) {
 		echo "<script>
-                alert('Registro de periodo academico Exitoso');
-                window.location= '../periodo_academicos.php'
+                alert('Registro Exitoso periodo academico');
+                window.location= '../periodo_academico.php'
     			</script>";
 	}else{
 		echo "<script>
-			alert('Error al Registrar periodo academico');
+			alert('Error al registrar periodo academico');
 			window.history.back();
 			</script>
 			";
