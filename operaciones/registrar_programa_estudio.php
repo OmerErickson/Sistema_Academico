46abfbd9d276d19fdbcc779ac0a38f887b8880b8
OmerErickson <omertaboadaiestph@gmail.com>
2022-12-14 16:27:40 -0500

    Update registrar_programa_estudio.php

--- operaciones/registrar_programa_estudio.php
+++ operaciones/registrar_programa_estudio.php
@@ -7,8 +7,8 @@ $tipo       = $_POST['tipo'];
 $nombre     = $_POST['nombre'];
 $resolucion = $_POST['resolucion'];
 
-$b_programa_estudios = buscarProgramaEstudio($conexion, $codigo);
-$c_r_b_programa_estudios = mysqli_num_rows($b_programa_estudios);
+$b_codigo = buscarProgramaEstudio($conexion, $codigo);
+$c_r_b_codigo = mysqli_num_rows($b_codigo);
 if ($c_r_b_programa_estudios == 0) {
 	$insertar = "INSERT INTO programa_estudios (codigo, tipo, nombre, resolucion) VALUES ('$codigo','$tipo','$nombre', '$resolucion')";
 	$ejecutar_insetar = mysqli_query($conexion, $insertar);
